b1b0ecc37c43d1fd5b2b8bac55c043c964b35d4b
Claudia <claudiamorenofa@gmail.com>
2025-03-17 10:27:49 +0100

    SCRIPT WITH CORRECTION. This script is able to classify correctly any RNA or DNA sequence

--- seqClass.py
+++ seqClass.py
@@ -14,15 +14,27 @@ if len(sys.argv) == 1:
 args = parser.parse_args()
 args.seq = args.seq.upper()
 
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
-        print ('The sequence is DNA')
-    elif re.search('U', args.seq):
-        print ('The sequence is RNA')
-    else:
-        print ('The sequence can be DNA or RNA')
-else:
-    print ('The sequence is not DNA nor RNA')
+#if re.search('^[ACGTU]+$', args.seq): # This pattern allows both T and U in the same sequence, which is incorrect
+ #   if re.search('T', args.seq):
+ #       print ('The sequence is DNA') 
+ #   elif re.search('U', args.seq):
+ #       print ('The sequence is RNA')
+ #   else:
+ #       print ('The sequence can be DNA or RNA')
+#else:
+ #   print ('The sequence is not DNA nor RNA')
+def search_DNA_RNA(seq):
+	if 'T' and 'U' in seq:
+		return "The sequence cannot contain both T and U"
+	elif 'T' in seq:
+		return "The sequence is DNA"
+	elif 'U' in seq:
+		return "The sequence is RNA"
+	elif re.fullmatch(r'^[ACG]+$', seq): # This is ambigous because it does not contain either T or U
+		return "I am not sure...The sequence can be either DNA or RNA"
+	else: # If there are numbers... or other symbol
+		return "The sequence is not DNA nor RNA"
+print(search_DNA_RNA(args.seq))
 
 if args.motif:
     args.motif = args.motif.upper()
